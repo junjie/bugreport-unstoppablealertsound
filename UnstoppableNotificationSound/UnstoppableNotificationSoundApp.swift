1943a507a38b94b4add2567c8c9076c5ae538b07
Junjie <junjie@dueapp.com>
2020-07-08 10:48:54 +0800

    Initial Commit

---
+++ UnstoppableNotificationSound/UnstoppableNotificationSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnstoppableNotificationSoundApp.swift
+//  UnstoppableNotificationSound
+//
+//  Created by Lin Junjie on 8/7/20.
+//
+
+import SwiftUI
+
+@main
+struct UnstoppableNotificationSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
